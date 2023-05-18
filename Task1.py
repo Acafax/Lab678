e607fe737a13fa94af6a1ab11f9d1fc7cfbea940
Acafax <131300683+Acafax@users.noreply.github.com>
2023-05-18 21:42:32 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -4,10 +4,9 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 
-def konwersja(source_path, dest_path):
-    source_ext = os.path.splitext(source_path)[1]
-    dest_ext = os.path.splitext(dest_path)[1]
-    
-source_path =  sys.argv[1]
-dest_path = sys.argv[2]
-konwersja(source_path, dest_path)
+source_path =  sys.argv[1] # ŚCIEZKA ŹRÓDŁOWA
+dest_path = sys.argv[2] #ściezka docelowa
+
+source_ext = os.path.splitext(source_path)[1] # czy źródłowa to json, yaml, xml 
+dest_ext = os.path.splitext(dest_path)[1] # czy docelowa to json, yaml, xml
+
